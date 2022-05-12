b83d36c6eff2d0f3043c00b29850cf3e7288c0a9
yapicimurat <muratyapiicii@gmail.com>
2022-05-12 23:53:16 +0300

    Gereksiz dosya silinmesi...

--- src/components/employee/annualLeaveRequest/AnnualLeaveRequestDetail.js
+++
@@ -1,9 +0,0 @@
-import SectionContent from "../../SectionContent";
-
-export default function AnnualLeaveRequestDetail(){
-    return (
-        <SectionContent content={
-            <h1>AnnualLeaveRequestDetail</h1>
-        }/>
-    );
-}
